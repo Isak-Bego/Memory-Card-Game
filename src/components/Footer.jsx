d8a8feb5556eea2f818619b97e72e2f20fd56499
Isak Bego <isakbego@gmail.com>
2024-01-31 04:47:43 +0200

    initial release 1.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 function Footer(){
     return (
-        <>
-        </>
+        <p>&copy; 2024 Isak Bego. All rights reserved. | <a href="mailto:isakbego@gmail.com">isakbego@gmail.com</a></p>
     );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
+import Logo from '../assets/Memory Game logo.webp';
+import '../styles/Header.css';
+
 function Header(){
     return (
-        <></>
+        <div className="header">
+            <div className="logoContainer">
+                <img src={Logo} alt="Memory Challenge Logo" />
+            </div>
+        </div>
     );
 }
 
