a18ad66f51feb64ac95255a94268e1b1f69f88e8
Isak Bego <isakbego@gmail.com>
2024-01-31 04:47:43 +0200

    initial release 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './styles/App.css'
 import Board from './components/Board.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
 
   return (
     <>
+      <Header />
       <Board />
+      <Footer />
     </>
   )
 }
